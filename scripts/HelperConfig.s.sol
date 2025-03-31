192047f54e714c3098f5a52b36cccecf9ac1e96b
pieakshat <akshat.05p@gmail.com>
2025-03-31 10:54:50 +0530

    userOp signature almost done

--- scripts/HelperConfig.s.sol
+++ scripts/HelperConfig.s.sol
@@ -1,7 +1,8 @@
 // SPDX-Lisence-Identifier: MIT
 pragma solidity 0.8.24; 
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {EntryPoint} from "lib/account-abstraction/contracts/core/EntryPoint.sol"; 
 
 contract HelperConfig is Script {
     error HelperConfig__InvalidChainId(); 
@@ -53,6 +54,12 @@ contract HelperConfig is Script {
         }
     
     // deploy mocks 
+    console2.log("Deploying mocks..."); 
+    vm.startBroadcast(FOUNDRY_DEFAULT_WALLET);
+    EntryPoint entryPoint = new EntryPoint(); 
+    
+    vm.stopBroadcast();
+
     return NetworkConfig({entryPoint: address(0), account: FOUNDRY_DEFAULT_WALLET}); 
     }
 
--- scripts/SendPackedUserOp.s.sol
+++ scripts/SendPackedUserOp.s.sol
@@ -2,12 +2,53 @@
 pragma solidity 0.8.24; 
 
 import {Script} from "forge-std/Script.sol"; 
-import {MinimalAccount} from "src/ethereum/MinimalAccount.sol"; 
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol"; 
+import {NetworkConfig} from "script/HelperConfig.s.sol"; 
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol"; 
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol"; 
+contract SendPackedUserOp is Script {
+    using MessageHashUtils for bytes32; 
+    function run() public {
 
-contract DeployMinimal is Script {
+    }
 
-    function run() public {} 
+    function generateSignedUserOperation(bytes memory callData, HelperConfig.NetworkConfig memory config) public returns (PackedUserOperation memory) {
+        // generate the unsigned data 
+        uint256 nonce = vm.getNonce(sender);
+        PackedUserOperation memory userOp = _generateUnsignedUserOperation(
+            callData, 
+            config.account, 
+            nonce 
+        );
 
-    function deployMinimalAccount() public {}
+        // Get the userOp hash
+        bytes32 userOpHash =  IEntryPoint(config.entryPoint).getUserOpHash(userOp);
+        bytes32 digest = userOpHash.toEthSignedMessageHash();  
 
+        // sign it and then return it 
+        // to sign this we need to get the userOperation hash that the entryPoint contract is looking for 
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(config.account, digest);
+        unsignedUserOp.signature = abi.encodePacked(r, s, v); 
+        return userOp; 
+    }
+
+    function _generateUnsignedUserOperation(bytes memory callData, address sender, uint256 nonce) internal pure returns(PackedUserOperation memory) {
+        uint128 verificationGasLimit = 16777216; 
+        uint128 callGasLimit = verificationGasLimit; 
+        uint128 maxPriorityFeePerGas = 256; 
+        uint128 maxFeePer = maxPriorityFeePerGas; 
+
+        // generate the unsigned data 
+        return PackedUserOperation({
+            sender: sender, 
+            nonce: nonce, 
+            initCode: hex"", 
+            callData: callData, 
+            accountGasLimits: bytes32(uint256(verificationGasLimit) << 128 | callGasLimit), 
+            preVerificationGas: verificationGasLimit, 
+            gasFees: bytes32(uint256(maxPriorityFeePerGas) << 128 | maxFeePerGas), 
+            paymasterAndData: hex"", 
+            signature: hex""
+        }); 
+    }
 }
